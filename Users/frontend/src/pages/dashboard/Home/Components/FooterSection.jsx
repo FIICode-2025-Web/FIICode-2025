cf3565f30c67cbd1f37d7fd320844b92d53703b2
Tudor Stroescu <tudorstroescu@yahoo.com>
2025-04-10 17:57:07 +0300

    More elegant feedback form

--- Users/frontend/src/pages/dashboard/Home/Components/FooterSection.jsx
+++ Users/frontend/src/pages/dashboard/Home/Components/FooterSection.jsx
@@ -4,7 +4,7 @@ import FeedbackForm from "./FeedbackForm";
 export function FooterSection() {
   return (
     <div className="bg-black text-gray-300 py-12 px-6">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-4xl mx-auto flex items-center justify-center flex-col">
         <div className="mb-12 text-center italic opacity-90">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam minima unde quisquam neque eveniet deserunt, hic ut! Accusantium laboriosam impedit dolorem assumenda adipisci omnis maiores. Voluptatibus harum quis possimus eius.
         </div>
